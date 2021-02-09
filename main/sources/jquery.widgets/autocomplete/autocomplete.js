44ba7fe8e18cb82ea1e64e36ad70ca88558624ca
Vladimir Daraduda <daraduda@outlook.com>
2021-02-09 11:31:29 +0100

    #modified the autocomplete widget and renamed folder for the dropdown widget

---
+++ main/sources/jquery.widgets/autocomplete/autocomplete.js
@@ -0,0 +1,38 @@
+$(function() {
+    $( "#tags-ajax-json" ).autocomplete({
+        source: function(request, response) {
+            $.ajax({
+                url: "http://api.geonames.org/searchJSON?q=UA&country=UA&lang=en&maxRows=10&username=slv_yaroslav",
+                dataType: "jsonp",
+                data: {
+                    featureClass: "P",
+                    style: "full",
+                    maxRows: 12,
+                    name_startsWith: request.term
+                },
+                success: function(data) {
+                    response($.map(data.geonames, function(item) {
+                        return {
+                            label: item.name + (item.adminName1 ? ", " + item.adminName1 : "") + ", " + item.countryName,
+                            value: item.name
+                        }
+                    }));
+                }
+            });
+
+        }
+    });
+    $( "#tags-json" ).autocomplete({
+        source: [
+            { label: "Saint Petersburg City", value: "Saint Petersburg" },
+            { label: "Moscow City", value: "Moscow" },
+            { label: "Kazan City", value: "Kazan" },
+            { label: "Samara City", value: "Samara" },
+            { label: "Omsk City", value: "Omsk" },
+            { label: "Ufa City", value: "Ufa" },
+            { label: "Penza City", value: "Penza" }
+        ],
+        open: function(event, ui) {
+        }
+    });
+});        
